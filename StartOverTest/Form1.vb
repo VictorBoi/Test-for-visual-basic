1041fc24443055b7722adfedad29cbc1e550bbba
VictorBoi <VictorMarquezYTA@Gmail.com>
2017-06-08 09:03:47 -0700

    Added Hi there.

--- StartOverTest/Form1.vb
+++ StartOverTest/Form1.vb
@@ -2,6 +2,6 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         System.Console.WriteLine("Hello World")
-
+        MessageBox.Show("Hi there")
     End Sub
 End Class
